2ad41b639a26599ec6aba74690929f734d6ba966
ishibashi.futos <ishibashi.futos@jp.fujitsu.com>
2020-08-21 22:28:40 +0900

    initial commit

---
+++ src/test/scala/com/github/fishibashi/nosqlexample/NoSQLExampleTest.scala
@@ -0,0 +1,12 @@
+package com.github.fishibashi.nosqlexample
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.junit.runner.RunWith
+import org.scalatestplus.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class NoSQLExampleTest extends AnyFunSuite {
+  test("someLibraryMethod is always true") {
+    assert(NoSQLExample.someLibraryMethod())
+  }
+}
