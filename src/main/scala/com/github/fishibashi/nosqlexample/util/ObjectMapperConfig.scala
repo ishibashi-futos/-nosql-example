2f587b9e9573f2a2897786d2053028080536f3ac
ishibashi.futos <ishibashi.futos@outlook.com>
2020-08-25 14:55:53 +0900

    ScalaObjectMapperをmixin

--- src/main/scala/com/github/fishibashi/nosqlexample/util/ObjectMapperConfig.scala
+++ src/main/scala/com/github/fishibashi/nosqlexample/util/ObjectMapperConfig.scala
@@ -1,13 +1,14 @@
 package com.github.fishibashi.nosqlexample.util
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.module.scala.{DefaultScalaModule, ScalaObjectMapper}
 
 sealed trait ObjectMapperConfig {
   def getObjectMapper: ObjectMapper
 }
 
 object DefaultMapperConfig extends ObjectMapperConfig {
-  private val mapper = new ObjectMapper().registerModule(DefaultScalaModule)
+  private val mapper = (new ObjectMapper() with ScalaObjectMapper).registerModule(DefaultScalaModule)
+
   override def getObjectMapper: ObjectMapper = mapper
 }
